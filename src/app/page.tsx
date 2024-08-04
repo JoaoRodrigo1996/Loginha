be9140996b90823a72e333cd836aad07c4809177
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-08-04 15:57:24 -0300

    feat: create product details page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
-import Image from "next/image";
+import { Heart, Search, ShoppingCart } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="space-y-6 max-w-7xl mx-auto">
+      <header className="flex items-center justify-between px-6 py-3">
+        <h1 className="text-2xl font-bold">JR.<span className="text-[#edcf5d]">M</span></h1>
+        <div className="h-[42px] rounded-full flex items-center justify-center gap-2 border px-4">
+          <label htmlFor="">
+            <Search className='size-4 text-zinc-500' />
+          </label>
+          <input type="text" className='bg-transparent focus:outline-none text-sm placeholder:text-zinc-500' placeholder="Pesquisar" />
         </div>
-      </div>
+        <div className="flex items-center gap-3">
+          <button className='p-3 border rounded-full' >
+            <ShoppingCart className="size-4" />
+          </button>
+          <button className='p-3 border rounded-full' >
+            <Heart className="size-4" />
+          </button>
+          <div className="size-[42px] rounded-full">
+            <img className='size-[42px] rounded-full' src="https://github.com/JoaoRodrigo1996.png" alt="" />
+          </div>
+        </div>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="px-6 space-y-6">
+        <menu className="border-b pb-4 flex items-center gap-5">
+          <a href="#" className="text-sm font-medium">Mulheres</a>
+          <a href="#" className="text-sm font-medium">Homens</a>
+          <a href="#" className="text-sm font-medium">Crianças</a>
+          <a href="#" className="text-sm font-medium">Esportes</a>
+          <a href="#" className="text-sm font-medium">Marcas</a>
+          <a href="#" className="text-sm font-medium">Novos</a>
+          <a href="#" className="text-sm font-medium">Oferta</a>
+        </menu>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+        <div className="flex gap-16">
+          <div className="flex-1">
+            <div className="">
+              <img src="/shoe.avif" alt="" className='object-cover rounded-xl' />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between flex-1 space-y-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <img src="/nike.svg" alt="" className="w-8" />
+                <span className="font-medium text-sm">Nike</span>
+              </div>
+              <span className="text-zinc-500">TE1254R00-,-1</span>
+            </div>
+            <div className="space-y-6">
+              <h4 className="text-xl font-semibold">Tênis Nike Court Vision Low Next Nature Masculino</h4>
+              <p className='text-3xl font-bold'>R$ 599.00</p>
+            </div>
+            <div className="space-y-2">
+              <span className="text-sm font-medium">Tamanhos</span>
+              <div className="flex flex-wrap items-center gap-3">
+                <button className="border px-5 py-2 rounded-lg">34</button>
+                <button className="border px-5 py-2 rounded-lg">35</button>
+                <button className="border px-5 py-2 rounded-lg">36</button>
+                <button className="border px-5 py-2 rounded-lg">37</button>
+                <button className="border px-5 py-2 rounded-lg">38</button>
+                <button className="border px-5 py-2 rounded-lg">39</button>
+                <button className="border px-5 py-2 rounded-lg">40</button>
+                <button className="border px-5 py-2 rounded-lg">41</button>
+                <button className="border px-5 py-2 rounded-lg">42</button>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <button className="w-full h-11 bg-black text-zinc-50 flex items-center justify-center gap-3 rounded-lg">
+                <ShoppingCart className='size-5' />
+                Adicionar ao carrinho
+              </button>
+              <button className='px-6 flex items-center justify-center h-11 rounded-lg bg-[#edcf5d]'>
+                <Heart className='size-5' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
